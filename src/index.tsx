ed6a42e1e06f651c3e2e13d9c9b72a30e9b4cb25
vitaly-redkin <v21267@gmail.com>
2020-04-05 11:26:42 +0300

    GA added

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,18 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as ReactGA from 'react-ga';
+
 
 import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
+
+const gaTrackingId: string = process.env.REACT_APP_GA_TRACKING_ID as string;
+ReactGA.initialize(gaTrackingId);
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
